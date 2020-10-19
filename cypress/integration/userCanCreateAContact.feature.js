b445b094455f53c9fffbcb6a6cd19efe599bb268
Kyungin Na <KyunginNa@1.local>
2020-10-19 20:35:00 +0200

    created a form named new-contact-form which users can input contact details and submit them, created div whose id is contact-list, and linked js file in index.html. Also, added tests for the form in userCanCreateAContact.feature.js

--- cypress/integration/userCanCreateAContact.feature.js
+++ cypress/integration/userCanCreateAContact.feature.js
@@ -1,6 +1,6 @@
 describe('user can create a contact', () => {
     it('test', () => {
-        cy.visit('http://localhost:3001')
+        cy.visit('http://localhost:3000')
         cy.get('#add-contact').click()
         cy.get('#name').type('Kyungin')
         cy.get('#email').type('nakyungin93@gmail.com')
@@ -8,5 +8,16 @@ describe('user can create a contact', () => {
         cy.get('#company').type('Craft Academy')
         cy.get('#notes').type('student')
         cy.get('#sns').type('TBD')
+        cy.get('#submit').click()
     })
+
+    it('displays a name of the new contact', () => {
+        cy.get('#contact-list').should('contain','Kyungin')
+    })
+
+    it('displays an email address of the new contact', () => {
+        cy.get('#contact-list').should('contain','nakyungin93@gmail.com')
+    }
+    )
+
 })
