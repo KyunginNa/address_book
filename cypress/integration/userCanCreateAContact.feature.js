15b87747bfedf4b567b58f1e96d7152eb845dad0
Kyungin Na <KyunginNa@1.local>
2020-10-19 15:51:37 +0200

    added a form named new-contact-form in index.html and added a test for this form in userCanCreateAContact.feature.js

--- cypress/integration/userCanCreateAContact.feature.js
+++ cypress/integration/userCanCreateAContact.feature.js
@@ -2,5 +2,11 @@ describe('user can create a contact', () => {
     it('test', () => {
         cy.visit('http://localhost:3001')
         cy.get('#add-contact').click()
+        cy.get('#name').type('Kyungin')
+        cy.get('#email').type('nakyungin93@gmail.com')
+        cy.get('#phone').type('1234 567890')
+        cy.get('#company').type('Craft Academy')
+        cy.get('#notes').type('student')
+        cy.get('#sns').type('TBD')
     })
 })
